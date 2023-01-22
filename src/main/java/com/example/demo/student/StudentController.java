c2874a6201dfde06a4f025c56a83a09766d02135
Elimasrojas <elimasrojas@gmail.com>
2023-01-22 12:49:53 -0500

    Student. controller,model

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,21 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "ap1/v1/students")
+public class StudentController {
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return Arrays.asList(
+                new Student(1l, "Elkin", "elkin@gmail.com", Gender.MALE),
+                new Student(2l, "Alex", "alex@gmail.com", Gender.FEMALE)
+        );
+    }
+
+}
